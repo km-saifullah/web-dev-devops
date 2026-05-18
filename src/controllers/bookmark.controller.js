c5fab37d186a13ed2915f30cb4b3dd2bce0b8853
km-saifullah <kmsaifullah16@gmail.com>
2026-05-18 22:13:07 +0600

    update a bookmark by id feature added

--- src/controllers/bookmark.controller.js
+++ src/controllers/bookmark.controller.js
@@ -10,7 +10,7 @@ export const getAllBookmark = async (req, res) => {
       data: bookmark,
     });
   } catch (error) {
-    return res.status(400).json({
+    return res.status(500).json({
       status: false,
       message: "Error while fetching the bookmark data",
       data: error.message,
@@ -39,7 +39,7 @@ export const createBookmark = async (req, res) => {
       data: bookmark,
     });
   } catch (error) {
-    return res.status(400).json({
+    return res.status(500).json({
       status: false,
       message: "Error while creating the bookmark data",
       data: error.message,
@@ -48,7 +48,58 @@ export const createBookmark = async (req, res) => {
 };
 
 // update a bookmark
-export const updateBookmark = async (req, res) => {};
+export const updateBookmark = async (req, res) => {
+  try {
+    const { bookmarkTitle, bookmarkUrl } = req.body;
+    const { id } = req.params;
+
+    // check the req.body is empty or not
+    if (!bookmarkTitle && !bookmarkUrl)
+      return res
+        .status(400)
+        .json({ status: false, message: "request body is empty", data: null });
+
+    // check if bookmark exists
+    const bookmarkExist = await Bookmark.findById(id);
+
+    if (!bookmarkExist) {
+      return res.status(404).json({
+        status: false,
+        message: "Bookmark does not exist",
+        data: null,
+      });
+    }
+
+    // build dynamic update object
+    const updateData = {};
+
+    if (bookmarkTitle !== undefined) updateData.bookmarkTitle = bookmarkTitle;
+    if (bookmarkUrl !== undefined) updateData.bookmarkUrl = bookmarkUrl;
+
+    // if no fields provided
+    if (Object.keys(updateData).length === 0) {
+      return res.status(400).json({
+        status: false,
+        message: "No fields provided to update",
+        data: null,
+      });
+    }
+
+    const updatedBookmark = await Bookmark.findByIdAndUpdate(id, updateData);
+
+    return res.status(200).json({
+      status: true,
+      message: "Bookmark updated successfully",
+      data: updatedBookmark,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: "Error while updating the bookmark",
+      data: error.message,
+    });
+  }
+};
 
 // delete a bookmark
 export const deleteBookmark = async (req, res) => {};
--- src/routes/bookmark.routes.js
+++ src/routes/bookmark.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   createBookmark,
   getAllBookmark,
+  updateBookmark,
 } from "../controllers/bookmark.controller.js";
 
 const router = Router();
 
 router.get("/status", getAllBookmark);
 router.post("/data", createBookmark);
+router.put("/update-bookmark/:id", updateBookmark);
 
 export default router;
